53f37e4d74101155c12bd08bc9249467d9b462be
SagarKapase <sagarkapase139@gmail.com>
2024-08-22 21:24:14 +0530

    integrated random jokes api

--- app/src/main/java/com/monoexpensetracker/ProfileFragment.kt
+++ app/src/main/java/com/monoexpensetracker/ProfileFragment.kt
@@ -5,15 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.monoexpensetracker.databinding.FragmentProfileBinding
+import com.monoexpensetracker.dataclass.JokeResponse
+import com.monoexpensetracker.objectpkg.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchRandomJoke()
+    }
+
+    private fun fetchRandomJoke()
+    {
+        RetrofitInstance.api.getRandomJoke().enqueue(object : Callback<JokeResponse>{
+            override fun onResponse(call: Call<JokeResponse>, response: Response<JokeResponse>) {
+                if (response.isSuccessful)
+                {
+                    val joke = response.body()?.joke
+                    binding.jokeTextView.text = joke ?:"No Joke Found! "
+                }else
+                {
+                    binding.jokeTextView.text = "Failed to load joke"
+                }
+            }
+
+            override fun onFailure(call: Call<JokeResponse>, t: Throwable) {
+                binding.jokeTextView.text = "Damn! Something went's Wrong"
+            }
+        })
     }
 
 }
